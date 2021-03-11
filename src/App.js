8b73d843208bc8236556ef47c35ec3e8ad5ba4bd
Crystal LaGrone <crystallagrone@Crystals-MacBook-Air.local>
2021-03-11 17:11:49 -0600

    text change

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const ukulele = "My first code"
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {ukulele}
         </p>
         <a
           className="App-link"
